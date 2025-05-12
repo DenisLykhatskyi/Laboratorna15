41b8cb278907671f6096c3c1e00076d3d6b57bea
DenisLykhatskyi <deneneson@gmail.com>
2025-05-12 23:00:01 +0300

    Laboratorna15
    Лабораторна робота №15

--- Lab15_1.c
+++ Lab15_1.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <conio.h>
 
-int main()
-{
-    int a = 3, b = 1;
-    int *a_ptr, *b_ptr;
+int main() {
+    float x, y;
+    float *x_ptr, *y_ptr;
 
-    a_ptr = &a;
-    b = *a_ptr;
-    b_ptr = &b;
-    *b_ptr = 2 * (*a_ptr);
+    x_ptr = &x;
+    *x_ptr = 5;
+    x = 5.5 + *x_ptr;
+    y_ptr = x_ptr;
+    y = *y_ptr / 2 + *x_ptr;
+    y_ptr = &y;
 
-    printf("Значение a=%d b=%d\n", a, b);
-    printf("Адреc a=%p b=%p\n", &a, &b);
-    printf("Значение ptr=%p ptr=%p\n", a_ptr, b_ptr);
-    printf("Значение по адресу a_ptr=%d.\n", *a_ptr);
-    printf("Значение по адресу b_ptr=%d.\n", *b_ptr);
-    printf("Адреc a_ptr=%p.\n", &a_ptr);
-    printf("адреc b_ptr=%p.\n", &b_ptr);
-
+    printf("The value of x= %f y=%f\n", x, y);
+    printf("The address of x = %p y = %p\n", (void*)&x, (void*)&y);
+    printf("The value of x_ptr = %p y_ptr = %p\n", (void*)x_ptr, (void*)y_ptr);
+    printf("The value that x_ptr points to is %f.\n", *x_ptr);
+    printf("The value that y_ptr points to is %f.\n", *y_ptr);
+    printf("The address of x_ptr is %p.\n", (void*)&x_ptr);
+    printf("The address of y_ptr is %p.\n", (void*)&y_ptr);
+    puts("================");
     getch();
     return 0;
 }
--- Lab15_2.c
+++ Lab15_2.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
-#include <conio.h>
 
 int main() {
-    float x, y;
-    float *x_ptr, *y_ptr;
+    int arr[10];
+    int *p, *pmax;
+    int i, n;
 
-    x_ptr = &x;
-    *x_ptr = 5;
-    x = 5.5 + *x_ptr;
-    y_ptr = x_ptr;
-    y = *y_ptr / 2 + *x_ptr;
-    y_ptr = &y;
+    printf("n=");
+    scanf("%d", &n);
 
-    printf("The value of x= %f y=%f\n", x, y);
-    printf("The address of x = %p y = %p\n", &x, &y);
-    printf("The value of x_ptr = %p y_ptr = %p\n", x_ptr, y_ptr);
-    printf("The value that x_ptr points to is %f.\n", *x_ptr);
-    printf("The value that y_ptr points to is %f.\n", *y_ptr);
-    printf("The address of x_ptr is %p.\n", &x_ptr);
-    printf("The address of y_ptr is %p.\n", &y_ptr);
-    puts("================\n");
+    if (n <= 0 || n > 10) {
+        printf(".Некоректна кількість елементів (має бути від 1 до 10).\n");
+        return 1;
+    }
 
-    getch();
+    printf("input value arr \n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    pmax = arr;
+
+    for (p = arr + 1; p < arr + n; p++) {
+        if (*p > *pmax) {
+            pmax = p;
+        }
+    }
+
+    printf("Максимальний елемент = %d \n", *pmax);
+    printf("Його число = %d \n", (int)(pmax - arr + 1));
+
     return 0;
 }
